e5422dbe016fcedb773dcc2986be97f61111d453
Cody Ferber <codyf86@gmail.com>
2018-06-02 11:31:09 -0600

    Update for james / E5

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter cedric montana owens perry,$(TARGET_DEVICE)),)
+ifneq ($(filter cedric james montana owens perry,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
